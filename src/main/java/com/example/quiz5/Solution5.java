46d3528e7cf92c1fb8266a5d44bbe21506448e61
Siddharth Rawat <rawat.sid@northeastern.edu>
2022-06-29 16:47:43 -0400

    :heavy_plus_sign: Solve quiz 5

--- src/main/java/com/example/quiz5/Solution5.java
+++ src/main/java/com/example/quiz5/Solution5.java
@@ -1,4 +1,61 @@
 package com.example.quiz5;
 
-public class Solution5 {
+import java.util.*;
+
+/**
+ * Implement QuickSort with T which is a generic type. You have to implement the sort and partition logic.
+ * You will need to add your code in the TODO section.
+ * There are 2 TODOs. they're in the sort and partition functions.
+ * You must not make any changes apart from the code.
+ * @param <T> Generic Type to run the QuickSort algorithm with generic data that is of type Comparable<T>.
+ */
+public class Solution5<T extends Comparable<T>> {
+    /*
+     * This method accepts a list of elements of generic type T which are comparable
+     * */
+    public void sort(List<T> a) {
+        sort(a, 0, a.size() - 1);
+    }
+
+    private  void sort(List<T> list, int lo, int hi) {
+        if (hi <= lo) return;
+        int pivot = partition(list, lo, hi);
+        //TODO
+        sort(list, lo, pivot-1);
+        sort(list, pivot+1, hi);
+    }
+
+    /*
+     *
+     * Implement the partition method that determines the pivot element.
+     * For T a and T b,
+     * a.compareTo(b) == 0 -> a == b;
+     * a.compareTo(b) < 0 -> a < b;
+     * a.compareTo(b) > 0 -> a > b
+     * */
+    public int partition(List<T> list, int lo, int hi) {
+        //TODO
+        T pivot = list.get(lo);
+
+        while (lo < hi) {
+            while (lo < hi && !less(list.get(hi), pivot))
+                hi--;
+            list.set(lo, list.get(hi));
+            while (lo < hi && less(list.get(lo), pivot))
+                lo++;
+            list.set(hi, list.get(lo));
+        }
+        list.set(lo, pivot);
+        return lo;
+    }
+
+    private void swap(List<T> list, int i, int j) {
+        T t = list.get(i);
+        list.set(i, list.get(j));
+        list.set(j, t);
+    }
+
+    private boolean less(T a, T b) {
+        return a.compareTo(b) < 0;
+    }
 }
--- src/test/java/com/example/quiz5/Solution5Test.java
+++ src/test/java/com/example/quiz5/Solution5Test.java
@@ -1,4 +1,92 @@
 package com.example.quiz5;
 
+import org.junit.Test;
+import org.junit.Assert;
+import org.junit.Before;
+
+import java.util.Arrays;
+import java.util.List;
+
 public class Solution5Test {
+    Solution5<Integer> qs = null;
+
+    @Before
+    public void init() {
+        qs = new Solution5<>();
+    }
+
+    @Test
+    public void partitionTest1() {
+        List<Integer> inputList = Arrays.asList(6,1,5,9,0,10,3,2);
+        Assert.assertTrue(checkPartition(inputList));
+    }
+
+    @Test
+    public void partitionTest2() {
+        List<Integer> inputList = Arrays.asList(1,3,2);
+        Assert.assertTrue(checkPartition(inputList));
+    }
+
+    @Test
+    public void partitionTest3(){
+        List<Integer> inputList = Arrays.asList(7,7,7,7,7,6);
+        Assert.assertTrue(checkPartition(inputList));
+    }
+
+    private boolean checkPartition(List<Integer> inputList){
+        int pivotIndex = qs.partition(inputList,0,inputList.size() - 1);
+        Integer pivot = inputList.get(pivotIndex);
+
+//            System.out.printf("pivot - inputList.get(%d) = %d\n", pivotIndex, pivot);
+//
+//            for(int i = 0; i < inputList.size(); i++){
+//                System.out.print(inputList.get(i) + ", ");
+//            }
+//
+//            System.out.println();
+
+        for(int i = 0; i <= pivotIndex; i++){
+            if(inputList.get(i).compareTo(pivot) > 0) return false;
+        }
+
+        for(int i = pivotIndex; i <= inputList.size() - 1; i++){
+            if(inputList.get(i).compareTo(pivot) < 0) return false;
+        }
+
+        return true;
+    }
+
+    @Test
+    public void quickSortRandomList() {
+        List<Integer> inputList = Arrays.asList(6,1,5,9,0,10,3,2);
+        List<Integer> expectedList = Arrays.asList(0,1,2,3,5,6,9,10);
+        qs.sort(inputList);
+        Assert.assertEquals(expectedList,inputList);
+    }
+
+    @Test
+    public void quickSortReverseSortedTest() {
+        List<Integer> inputList = Arrays.asList(10,9,8,7);
+        List<Integer> expectedList = Arrays.asList(7,8,9,10);
+        qs.sort(inputList);
+
+        Assert.assertEquals(expectedList,inputList);
+    }
+
+    @Test
+    public void quickSortSortedTest() {
+
+        List<Integer> inputList = Arrays.asList(1,2,3,4,5,6,7,8,9,10);
+        List<Integer> expectedList = Arrays.asList(1,2,3,4,5,6,7,8,9,10);
+        qs.sort(inputList);
+        Assert.assertEquals(expectedList,inputList);
+    }
+
+    @Test
+    public void quickSortedDuplicateElementsTest() {
+        List<Integer> inputList = Arrays.asList(7,4,1,2,3,3,7,9,8);
+        List<Integer> expectedList = Arrays.asList(1,2,3,3,4,7,7,8,9);
+        qs.sort(inputList);
+        Assert.assertEquals(expectedList,inputList);
+    }
 }
